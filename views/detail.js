f4b3c02b8fe923d8fbcff2824917eaa8a1a55ba6
fikarsuwardi <fikarsuwardi@gmail.com>
2024-08-22 00:33:52 +0700

    learn using components

--- views/detail.js
+++ views/detail.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default function DetailsScreen() {
+
+  function HelloWorld(props) {
+    return (
+      <Text>Test {props.name} {props.place}</Text>
+    )
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <HelloWorld name="res" place="ss"></HelloWorld>
       <Text>Details Screen</Text>
     </View>
   );
--- views/home.js
+++ views/home.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
+    const [count, setCount] = React.useState(0);
+
+//   React.useEffect(() => {
+//     document.title = `You clicked ${count} times`;
+//   });
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home Screen</Text>
